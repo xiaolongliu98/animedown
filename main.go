95b419929fe349b351664a47f69f09cee8d34f45
xiaolong <1006814807@qq.com>
2024-01-02 00:09:09 +0800

    init commit

--- main.go
+++ main.go
@@ -7,21 +7,6 @@ import (
 	"fmt"
 )
 
-const (
-	GuideText = `Welcome to use AnimeDown! 
-Now, you can use the following commands to download anime:
-  1. Search anime by name: "s [key1] [key2] ..."
-  2. Next page: "np"
-  3. Previous page: "pp"
-  4. Download anime by magnet: "d [index] [OPTIONAL, target_dir, empty is '.']", 
-     index is the row of the current search result.
-
-  5. Exit: "q"
-  6. Clear screen: "cls"
-
-Please enter your command:`
-)
-
 func main() {
 	const intro = `*** Welcome to use AnimeDown! *** 
  - AnimeDown is designed to download anime easily from DongManHuaYuan.
